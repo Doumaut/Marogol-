06fba3d796016160f0edfa0785fc2e73877d2cf1
Doumaut <108480751+Doumaut@users.noreply.github.com>
2022-07-23 17:19:25 +0700

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -1528,7 +1528,7 @@ exports.rules = (prefix) => {
 ➡️ Mungkin dipengaruhi oleh jaringan, signal, banned oleh Whatsapp dan beberapa asalan.
 
 🗯️ Dimana saya bisa mendapatkan Script dari bot ini ?
-➡️ Script ini bisa kalian dapatkan di YouTube ZEEONE OFC
+➡️ Script ini di private oleh owner
 
 🗯️ Boleh saya menambah ke grup?
 ➡️ Untuk itu tergantung dari owner bot sendiri
